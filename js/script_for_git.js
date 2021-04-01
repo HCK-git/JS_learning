e4f4a49da65cf65aebbb257b9138b15d5092be0a
Kirill <matanoff.kirill@yandex.ru>
2021-04-01 17:11:59 +0300

    Third practice, functional programmming

--- js/script_for_git.js
+++ js/script_for_git.js
@@ -1,2 +1,17 @@
 "use strict";
 
+const string = "String with some interesting information";
+
+console.log(string.indexOf("information"));
+
+const logg = "important information";
+
+console.log(logg.slice(logg.indexOf(" ") + 1, logg.length));
+
+console.log(logg.substr());
+
+const num = 12.2;
+console.log(Math.round(num));
+
+const test = "12.2px";
+console.log(typeof(parseFloat(test)));
